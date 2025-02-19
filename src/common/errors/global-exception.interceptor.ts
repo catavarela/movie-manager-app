747540b25c131c51a4cfb3e2728fb8ee42ffabff
Catalina Varela Ballesteros <catavarela@Air-de-Catalina.fibertel.com.ar>
2025-02-19 18:08:12 -0300

    Global HTTP Exceptions handler

---
+++ src/common/errors/global-exception.interceptor.ts
@@ -0,0 +1,29 @@
+import {
+  CallHandler,
+  ConflictException,
+  ExecutionContext,
+  Injectable,
+  NestInterceptor,
+} from '@nestjs/common';
+import { catchError, Observable, throwError } from 'rxjs';
+import { NotFoundException } from '@nestjs/common';
+import { DuplicatedResourceException, ResourceNotFoundException } from './exceptions';
+
+@Injectable()
+export class GlobalExceptionInterceptor implements NestInterceptor {
+  intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
+    return next.handle().pipe(
+      catchError((error) => {
+        if (error instanceof ResourceNotFoundException) {
+          return throwError(() => new NotFoundException(error.message));
+        }
+
+        if (error instanceof DuplicatedResourceException) {
+          return throwError(() => new ConflictException(error.message));
+        }
+
+        return throwError(() => error);
+      }),
+    );
+  }
+}
